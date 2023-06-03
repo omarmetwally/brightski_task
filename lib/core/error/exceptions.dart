94b22ee7a6319efdecb119b68c64596bf69076ae
Omar <omarmetwally2008@hotmail.com>
2023-06-03 19:30:15 +0300

    error_Handling_Class

---
+++ lib/core/error/exceptions.dart
@@ -0,0 +1,9 @@
+class ServerException implements Exception{
+
+  //if i've any server exceptions will be implemented here
+}
+
+class LocalException implements Exception{
+
+  //if i've any Local exceptions will be implemented here
+}
